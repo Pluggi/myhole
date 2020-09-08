7b71e1f9bc0a8d0cfaf7455d8c7a40bf18a9a539
Pluggi <pluggi512@gmail.com>
2020-09-08 15:50:45 +0200

    dhcpd: Set user/group arguments to nobody
    Signed-off-by: Pluggi <pluggi512@gmail.com>

--- dhcpd/docker-entrypoint.sh
+++ dhcpd/docker-entrypoint.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 DHCPD_LEASES=/tmp/dhcpd.leases
 
 touch "${DHCPD_LEASES}"
-exec dhcpd -4 -f -d --no-pid -cf /etc/dhcpd.conf -lf "${DHCPD_LEASES}"
+exec dhcpd -user nobody -group nobody -4 -f -d --no-pid -cf /etc/dhcpd.conf -lf "${DHCPD_LEASES}"
