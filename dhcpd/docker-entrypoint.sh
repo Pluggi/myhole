206c870645b6ea272a75e548b65b02b8faeb0abf
Pluggi <pluggi512@gmail.com>
2020-09-08 15:44:30 +0200

    dhcpd: Create dhcp leases file
    Signed-off-by: Pluggi <pluggi512@gmail.com>

--- dhcpd/docker-entrypoint.sh
+++ dhcpd/docker-entrypoint.sh
@@ -2,4 +2,7 @@
 
 set -euxo pipefail
 
-exec dhcpd -4 -f -d --no-pid -cf /etc/dhcpd.conf -lf /tmp/dhcpd.leases
+DHCPD_LEASES=/tmp/dhcpd.leases
+
+touch "${DHCPD_LEASES}"
+exec dhcpd -4 -f -d --no-pid -cf /etc/dhcpd.conf -lf "${DHCPD_LEASES}"
